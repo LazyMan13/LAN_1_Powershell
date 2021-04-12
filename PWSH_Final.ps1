4ab3bb96caf48bec3ca4db069095a22ac05c2f25
LazyMan13 <rwesleyhenderson@gmail.com>
2021-04-12 11:43:33 -0500

    Add files via upload

---
+++ PWSH_Final.ps1
@@ -0,0 +1,24 @@
+﻿$hostname = $env:COMPUTERNAME
+$user = $env:USERNAME.Split(".")
+$fName = ( Get-Culture ).TextInfo.ToTitleCase( $user[0].ToLower() )
+$lName = ( Get-Culture ).TextInfo.ToTitleCase( $user[1].ToLower() )
+
+Function Set-ConsoleColor ($bc, $fc) {
+    $Host.UI.RawUI.BackgroundColor = $bc
+    $Host.UI.RawUI.ForegroundColor = $fc
+    Clear-Host
+}
+
+
+Set-ConsoleColor 'DarkCyan' 'Yellow'
+
+$bgcolor = Read-Host "What color would you like your background?"
+$textcolor = Read-Host "What color would you like your text?"
+
+Set-ConsoleColor $bgcolor $textcolor
+
+Clear-Host
+
+Write-Host "Hello $hostname`nBut that's not your real name is it $fName $($lName)?`n"
+$nameChoice = Read-Host "May I call you $($fName)?"
+
