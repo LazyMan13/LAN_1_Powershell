d424db28d451a54f01fb61f4973f25601c8c40a5
LazyMan13 <rwesleyhenderson@gmail.com>
2021-04-14 11:14:33 -0500

    Add files via upload
    update, not much to look at

--- PWSH_Final.ps1
+++ PWSH_Final.ps1
@@ -22,3 +22,10 @@ Clear-Host
 Write-Host "Hello $hostname`nBut that's not your real name is it $fName $($lName)?`n"
 $nameChoice = Read-Host "May I call you $($fName)?"
 
+if ($nameChoice.Substring(0,1).ToLower().Equals("y")) {
+    Write-Host "Great, Hello $fName"
+}
+else {
+    $fName = Read-Host "Oh? What may I call you then? "
+    Write-Host "Okay, you want to be called $fName"
+}
