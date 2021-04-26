e7e57d63fd3cb2581522164565c2d8b14108308a
LazyMan13 <rwesleyhenderson@gmail.com>
2021-04-26 10:45:39 -0500

    Add files via upload

--- powershell_lab_3.ps1
+++ powershell_lab_3.ps1
@@ -4,13 +4,13 @@ $users = Get-ADUser -Filter * -Properties Department | Select-Object `
 SamAccountName, Department | Where-Object Department -EQ $null
 
 if($users.SamAccountName.count -ne 0) {
-        foreach($item in $users) {
-            #Write-Host $item.SamAccountName $item.Department -ForegroundColor Yellow
-            Set-ADUser -Identity $item.SamAccountName -Department "Marketing"
-            Get-ADUser -Identity $item.SamAccountName -Properties Department | Select-Object name, Department
-        }
-    } else {
-        write-host "All Users SET"
+    foreach($item in $users) {
+        #Write-Host $item.SamAccountName $item.Department -ForegroundColor Yellow
+        Set-ADUser -Identity $item.SamAccountName -Department "Marketing"
+        Get-ADUser -Identity $item.SamAccountName -Properties Department | Select-Object name, Department
     }
+} else {
+    write-host "All Users SET"
+}
     
 
